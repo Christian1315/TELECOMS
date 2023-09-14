f7c45cc98d6da74df6b894e54369d17de6b9107e
Christian <gogochristian009@gmail.com>
2023-09-14 17:35:32 +0100

    Mail template revu

--- app/Http/Controllers/Api/V1/USER_HELPER.php
+++ app/Http/Controllers/Api/V1/USER_HELPER.php
@@ -163,7 +163,9 @@ class USER_HELPER extends BASE_HELPER
 
         #===== ENVOIE D'SMS AU USER DU COMPTE =======~####
 
-        $compte_msg = "Votre compte a été crée avec succès sur FRIK-SMS. Voici ci-dessous vos identifiants de connexion: Username::" . $username . "   Password: " . $formData["password"];
+        // $compte_msg = "Votre compte a été crée avec succès sur FRIK-SMS. Voici ci-dessous vos identifiants de connexion: Username::" . $username . "   Password: " . $formData["password"];
+        $compte_msg = "Votre compte a été crée avec succès sur FRIK-SMS";
+
         $account_activation_code = "https://telecoms.digital/auth/register/activate?code=" . $active_compte_code;
         $compte_activation_msg = "Votre compte n'est pas encore actif. Veuillez l'activer en utilisant le ci-dessous : " . $account_activation_code;
 
--- resources/views/emails/sendEmail.blade.php
+++ resources/views/emails/sendEmail.blade.php
@@ -4,7 +4,7 @@
 <head style="font-family: Inter;">
     <meta charset="UTF-8" style="font-family: Inter;">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" style="font-family: Inter;">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title style="font-family: Inter;">FRIK-SMS</title>
 
 
@@ -20,12 +20,15 @@
             <div class="col-md-8" id="content" style="font-family: Inter;background: #fff;border-radius: 10px 10px 0px 0px;box-shadow: 0 1rem 3rem rgba(0, 0, 0, .175) !important;">
                 <!-- main -->
                 <div class="main" style="font-family: Inter;padding: 30px;">
-                    <h1 class="text-center text-dark title" style="font-family: Inter;font-size: 33px;font-weight: 400;line-height: 64px;letter-spacing: 0.125em;color: #000;text-align: center !important;">{{$details["subject"]}}</h1>
+                    <h1 class="text-center text-dark title" style="font-family: Inter;font-size: 33px;font-weight: 400;line-height: 30px!important;color: #000;text-align: center !important;">{{$details["subject"]}}</h1>
                     <p class="text-center" style="font-family: Inter;font-size: 22px;font-weight: 400;line-height: 27px;letter-spacing: 0em;margin-block: 30px;text-align: center !important;">Nous sommes heureux de vous compter parmi les utilisateurs de frikSMS .</p>
+                    <br>
                     <p class="" style="font-family: Inter;font-size: 22px;font-weight: 400;line-height: 27px;letter-spacing: 0em;margin-block: 30px;"> {{$details["message"]}}</p>
+                    <br>
                     <p class="text-center" style="font-family: Inter;font-size: 22px;font-weight: 400;line-height: 27px;letter-spacing: 0em;margin-block: 30px;text-align: center !important;">
                         <a href="https://telecoms.digital" class="btn btn-md text-white confirm-btn" style="font-family: Inter;padding: 20px;border-radius: 5px;color: #fff;font-size: 22px;font-weight: 400;line-height: 27px;letter-spacing: 0em;margin-block: 50px!important;background: #730208!important;box-shadow: 0 1rem 3rem rgba(0, 0, 0, .175) !important;text-align: center !important;text-decoration:none!important">Visitez le site</a>
                     </p>
+                    <br>
                     <p class="" style="font-family: Inter;font-size: 22px;font-weight: 400;line-height: 27px;letter-spacing: 0em;margin-block: 30px;">Si le bouton ne marche pas , copiez ce lien dans votre navigateur</p>
 
                     <a href="https://telecoms.digital" class="link" style="font-family: Inter;color: #730208;font-size: 18px;font-weight: 600;line-height: 22px;letter-spacing: 0em;text-decoration: none;">https://telecoms.digital</a>
@@ -40,7 +43,9 @@
                 <!-- footer -->
                 <div class="row shadow-lg py-3 text-center bottom" style="font-family: Inter;background: #73020866;box-shadow: 0 1rem 3rem rgba(0, 0, 0, .175) !important;text-align: center !important;">
                     <div class="col-md-12" style="font-family: Inter;padding-block: 10px!important">
+                        <br>
                         <h3 class="text-dark" style="font-family: Inter;color: #000;font-size: 18px;font-weight: 600;line-height: 22px;letter-spacing: 0em;">© Copyright 2023 - Développé par HSMC</h3>
+                        <br>
                     </div>
                 </div>
             </div>
