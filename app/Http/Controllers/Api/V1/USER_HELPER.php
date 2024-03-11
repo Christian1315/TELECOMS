96d304a2b957d43baccc41aa15d95a4d3e8f7d3f
Christian <gogochristian009@gmail.com>
2024-03-11 18:07:15 +0100

    envoie d'sms revu 3

--- app/Http/Controllers/Api/V1/USER_HELPER.php
+++ app/Http/Controllers/Api/V1/USER_HELPER.php
@@ -215,7 +215,7 @@ class USER_HELPER extends BASE_HELPER
 
             $user['rang'] = $user->rang;
             $user['profil'] = $user->profil;
-            $user['solde'] = Solde::with(["manager"])->where(["owner" => $user->id, "visible" => 1])->latest();
+            $user['solde'] = Solde::with(["manager"])->where(["owner" => $user->id, "visible" => 1])->orderBy("id","desc")[0];
             $user['developer'] = $user->developer;
             $user['expeditors'] = $user->expeditors;
             $user['token'] = $token;
